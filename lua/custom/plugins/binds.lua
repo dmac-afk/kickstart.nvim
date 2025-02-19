99235e0a3ae8da92e56c113e610d6c81896f496c
Douglas MacColl <douglasm2009@live.co.uk>
2025-02-19 00:48:54 +0000

    undotree

--- lua/custom/plugins/binds.lua
+++ lua/custom/plugins/binds.lua
@@ -1,8 +1,10 @@
 return {
-  vim.keymap.set('n', '<leader><Tab>', vim.cmd.NvimTreeToggle),
+  vim.keymap.set('n', '<leader><Tab>', vim.cmd.NvimTreeToggle, { desc = 'Toggle File Tree' }),
 
   vim.keymap.set('n', '<C-e>', ':lua vim.diagnostic.goto_next() <CR>'),
   vim.keymap.set('n', '<C-q>', ':lua vim.diagnostic.goto_prev() <CR>'),
 
   vim.keymap.set('n', '<F5>', ':colorscheme pywal16 <CR>'),
+
+  vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle [U]ndotree' }),
 }
