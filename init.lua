340bdc6cc2e29cca1cbc949fb02246d512e2c483
Douglas MacColl <douglasm2009@live.co.uk>
2025-09-05 22:17:55 +0100

    fix for parcel bundler not refreshing on update

--- init.lua
+++ init.lua
@@ -175,6 +175,8 @@ vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
+vim.o.backupcopy = 'yes'
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
